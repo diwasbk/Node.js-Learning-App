1c97d59838896f4514fe0fbe94c90030693e4868
diwasbk <diwasbk903@gmail.com>
2025-09-18 16:04:28 +0545

    Add try-catch to All Routes for Error Handling

--- crudAPI.js
+++ crudAPI.js
@@ -24,64 +24,84 @@ client.connect().then((connection) => {
 
     // Define GET API endpoint to fetch all students
     app.get("/api/students", async (req, res) => {
-        // Fetch all documents from the "students" collection
-        const result = await collection.find().toArray()
+        try {
+            // Fetch all documents from the "students" collection
+            const result = await collection.find().toArray()
 
-        // Send response back to client
-        res.send({
-            message: "Students Fetched Successfully!",
-            result: result,
-            success: true
-        })
+            // Send response back to client
+            res.send({
+                message: "Students Fetched Successfully!",
+                result: result,
+                success: true
+            })
+        } catch (err) {
+            console.log(err)
+        }
     })
 
     // GET Students by ID
     app.get("/api/students/:id", async (req, res) => {
-        const result = await collection.findOne({ _id: new ObjectId(req.params.id) })
-        if (!result) {
-            return res.send({
-                message: "Student Not Found",
-                success: false
+        try {
+            const result = await collection.findOne({ _id: new ObjectId(req.params.id) })
+            if (!result) {
+                return res.send({
+                    message: "Student Not Found",
+                    success: false
+                })
+            }
+            res.send({
+                message: `Welcome! ${result.name}`,
+                result: result,
+                success: true
             })
+        } catch (err) {
+            console.log(err)
         }
-        res.send({
-            message: `Welcome! ${result.name}`,
-            result: result,
-            success: true
-        })
     })
 
     // POST API to Enroll New Student
     app.post("/api/students/enroll", async (req, res) => {
-        const result = await collection.insertOne(req.body)
-        res.send({
-            message: "New Student Enrolled",
-            result: result,
-            success: true
-        })
+        try {
+            const result = await collection.insertOne(req.body)
+            res.send({
+                message: "New Student Enrolled",
+                result: result,
+                success: true
+            })
+        } catch (err) {
+            console.log(err)
+        }
     })
 
     // PUT API to Update Student Details
     app.put("/api/students/update/:id", async (req, res) => {
-        const result = await collection.updateMany(
-            { _id: new ObjectId(req.params.id) },
-            { $set: req.body }
-        )
-        res.send({
-            message: "Student Details Updated Successfully",
-            result: result,
-            success: true
-        })
+        try {
+            const result = await collection.updateMany(
+                { _id: new ObjectId(req.params.id) },
+                { $set: req.body }
+            )
+            res.send({
+                message: "Student Details Updated Successfully",
+                result: result,
+                success: true
+            })
+        } catch (err) {
+            console.log(err)
+        }
     })
 
     // DELETE API to Delete Student
     app.delete("/api/students/delete/:id", async (req, res) => {
-        const result = await collection.deleteMany({ _id: new ObjectId(req.params.id) })
-        res.send({
-            message: "Student Deleted Successfullly",
-            result: result,
-            succ: true
-        })
+        try {
+            const result = await collection.deleteMany({ _id: new ObjectId(req.params.id) })
+            res.send({
+                message: "Student Deleted Successfullly",
+                result: result,
+                succ: true
+            })
+        } catch (err) {
+            console.log(err)
+        }
     })
 })
 
